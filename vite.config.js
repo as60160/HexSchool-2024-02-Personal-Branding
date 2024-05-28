ac588ff9edea6436b545d75453544146e247fbfd
Jane Lin <jane.lin@imbrace.co>
2024-05-29 00:59:02 +0800

    chore: change to use 3000 port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // change to use 3000 port
+  server: {
+    port: 3000,
+  }
 })
